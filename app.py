4dc05d0ea7d1fd2c20ad5c0af65925931147545b
msugano <makoto.sugano@gmail.com>
2025-01-02 18:06:16 +0900

    adding examples.

--- app.py
+++ app.py
@@ -5,12 +5,16 @@ demo = gr.Interface(
     fn=process_document,
     inputs=gr.Image(type='filepath'),
     outputs=[
-             gr.Image(label="docai-image"),
-             gr.Text(label="docai-summary"),
-             gr.Markdown(label="vertex-summary"),
-             ],
+        gr.Image(label="docai-image"),
+        gr.Textbox(label="docai-summary"),
+        gr.Textbox(label="vertex-summary"),
+    ],
+    examples=[
+        ["/app/cloud-storage/image1.jpg"],
+        ["/app/cloud-storage/image2.jpg"]
+    ]
     #allow_flagging="never" 
 )
 
-demo.launch(share=True)
+demo.launch(share=True, allowed_paths=["/app/cloud-storage"])
 
